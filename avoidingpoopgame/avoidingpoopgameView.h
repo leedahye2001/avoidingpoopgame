dfd2f9679ba7e891951795b1da8b3c66feed3039
leedahye2001 <leedahye2001@naver.com>
2021-11-19 17:06:44 +0900

    초기 커밋.

---
+++ avoidingpoopgame/avoidingpoopgameView.h
@@ -0,0 +1,49 @@
+﻿
+// avoidingpoopgameView.h: CavoidingpoopgameView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CavoidingpoopgameView : public CView
+{
+protected: // serialization에서만 만들어집니다.
+	CavoidingpoopgameView() noexcept;
+	DECLARE_DYNCREATE(CavoidingpoopgameView)
+
+// 특성입니다.
+public:
+	CavoidingpoopgameDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual void OnDraw(CDC* pDC);  // 이 뷰를 그리기 위해 재정의되었습니다.
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 구현입니다.
+public:
+	virtual ~CavoidingpoopgameView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // avoidingpoopgameView.cpp의 디버그 버전
+inline CavoidingpoopgameDoc* CavoidingpoopgameView::GetDocument() const
+   { return reinterpret_cast<CavoidingpoopgameDoc*>(m_pDocument); }
+#endif
+
